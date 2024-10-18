741a3441a26378af9ad39a2e2ca30cdd4fe17f45
AchiGel <ar.gelashvili@gmail.com>
2024-10-18 13:12:39 +0400

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,26 @@
 import styles from "./Footer.module.css";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin, FaRegEnvelope } from "react-icons/fa";
+import {} from "react-icons/fa";
 
 export default function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <h2>Contact</h2>
+      <p>
+        Seasoned Full Stack Software Engineer with over 8 years of hands-on
+        experience in designing and implementing robust, scalable, and
+        innovative web solutions. Adept at leveraging a comprehensive skill set
+        encompassing front-end and back-end technologies
+      </p>
+      <span>
+        <FaRegEnvelope /> Ar.gelashvili@gmail.com
+      </span>
+      <div className={styles.icons}>
+        <FaInstagram />
+        <FaXTwitter />
+        <FaLinkedin />
+      </div>
+    </div>
+  );
 }
