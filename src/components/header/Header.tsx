114008835ec82c1283040cbf9e1c7ae7ba95c95d
AchiGel <ar.gelashvili@gmail.com>
2024-10-28 16:27:15 +0400

    add nav links color change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,64 @@
+import { useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const [activeLink, setActiveLink] = useState("Home");
+
+  const changeColor = (link: string) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div className={styles.header}>
+    <div id="header" className={styles.header}>
       <nav>
         <ul>
-          <li>Home</li>
+          <a href="#header">
+            <li
+              className={
+                activeLink === "Home"
+                  ? styles.nav_link_clicked
+                  : styles.nav_link
+              }
+              onClick={() => changeColor("Home")}
+            >
+              Home
+            </li>
+          </a>
           <a href="#aboutMe">
-            <li>About me</li>
+            <li
+              className={
+                activeLink === "AboutMe"
+                  ? styles.nav_link_clicked
+                  : styles.nav_link
+              }
+              onClick={() => changeColor("AboutMe")}
+            >
+              About me
+            </li>
           </a>
           <a href="#projects">
-            <li>Projects</li>
+            <li
+              className={
+                activeLink === "Projects"
+                  ? styles.nav_link_clicked
+                  : styles.nav_link
+              }
+              onClick={() => changeColor("Projects")}
+            >
+              Projects
+            </li>
           </a>
           <a href="#contact">
-            <li>Contact</li>
+            <li
+              className={
+                activeLink === "Contact"
+                  ? styles.nav_link_clicked
+                  : styles.nav_link
+              }
+              onClick={() => changeColor("Contact")}
+            >
+              Contact
+            </li>
           </a>
         </ul>
       </nav>
